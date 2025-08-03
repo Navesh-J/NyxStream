b7ee9f2bb82ffd174b07ce0c19f1b754c47dad98
Navesh Jaiswal <navesh.professional@gmail.com>
2025-08-03 20:18:21 +0530

    Debug

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -27,7 +27,7 @@ import { NextRequest, NextResponse } from "next/server";
 // }
 
 export async function GET() {
-  return NextResponse.json({ ok: true })
+  return NextResponse.json([], { status: 200 })
 }
 
 export async function POST(request: NextRequest) {
